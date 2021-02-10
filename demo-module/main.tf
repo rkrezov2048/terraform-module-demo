b0b18427286e469e30bc1561a9960d0b184520ce
rkrezov2048 <rkrezov@gmail.com>
2021-02-10 14:12:50 +0100

    added data source

--- demo-module/main.tf
+++ demo-module/main.tf
@@ -1,3 +1,6 @@
+data "aws_availability_zones" "available" {}
+
+
 resource "aws_vpc" "main" {
     cidr_block = var.cidr_block
 
@@ -10,21 +13,21 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main_public" {
   count = length(var.public_cidrs)
   vpc_id = aws_vpc.main.id
-  availability_zone = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"][count.index]
+  availability_zone = data.aws_availability_zones.available.names [count.index]
   cidr_block = var.public_cidrs[count.index]
   map_public_ip_on_launch = true
   tags = {
-    "Name" = "main_public_${count.index + 1}"
+    "Name" = "main_public_${data.aws_availability_zones.available.names[count.index]}"
   }
 }
 
 resource "aws_subnet" "main_private" {
   count = length(var.private_cidrs)
   vpc_id = aws_vpc.main.id
-  availability_zone = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"][count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   cidr_block = var.private_cidrs[count.index]
   map_public_ip_on_launch = false
   tags = {
-    "Name" = "main_private_${count.index + 1}"
+    "Name" = "main_private_${data.aws_availability_zones.available.names[count.index]}"
   }
 }
