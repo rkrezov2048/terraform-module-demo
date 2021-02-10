ebb90a3747a2117c1a14e1d07dc7331596e52c8a
rkrezov2048 <rkrezov@gmail.com>
2021-02-10 09:54:46 +0100

    initial module

---
+++ demo-module/main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "main" {
+    cidr_block = var.cidr_block
+
+    tag = {
+        Name = "Demo"
+    }
+
+}
+
+resource "aws_subnet" "example" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = var.availability_zone
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, 10)
+}
---
+++ demo-module/variables.tf
@@ -0,0 +1,7 @@
+variable "cidr_block" {
+  default = "172.16.0.0/16"
+}
+
+variable "availability_zone" {
+  default = "us-east-1"
+}
