4f99ad0273e5400eb974880a386bdf89fad1d572
rkrezov2048 <rkrezov@gmail.com>
2021-02-11 14:11:02 +0100

    security group

--- loadbalancer/main.tf
+++ loadbalancer/main.tf
@@ -3,7 +3,7 @@
 resource "aws_lb" "main_lb" {
     name = "main-alb"
     subnets = var.public_subnets
-    security_groups = [ var.public_sg ]
+    security_groups = [var.public_sg]
     idle_timeout = 400
   
 }
