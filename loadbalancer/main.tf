d55985bb2c06bd71deb431b027e89589c5c3f1b0
rkrezov2048 <rkrezov@gmail.com>
2021-02-11 14:07:22 +0100

    hypen checked

--- loadbalancer/main.tf
+++ loadbalancer/main.tf
@@ -1,7 +1,7 @@
 #  --- loadbalancer/main ----
 
 resource "aws_lb" "main_lb" {
-    name = "main_alb"
+    name = "main-alb"
     subnets = var.public_subnets
     security_groups = [ var.public_sg ]
     idle_timeout = 400
