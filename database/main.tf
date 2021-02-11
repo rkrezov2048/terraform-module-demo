5d19182d706782eddfcc25db7d5f5435d4b2c439
rkrezov2048 <rkrezov@gmail.com>
2021-02-11 12:38:00 +0100

    fixed var

--- database/main.tf
+++ database/main.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "main_db" {
   allocated_storage      = var.db_storage
   engine                 = "mysql"
   engine_version         = var.db_engine_version
-  instance_class         = var.instance_class
+  instance_class         = var.db_instance_class
   name                   = var.db_name
   username               = var.db_username
   password               = var.db_password
