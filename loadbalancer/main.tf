991cd1e44460a6b2658ae44df784d1da6225059a
rkrezov2048 <rkrezov@gmail.com>
2021-02-11 15:43:12 +0100

    loadbalancer module extend

--- loadbalancer/main.tf
+++ loadbalancer/main.tf
@@ -1,9 +1,24 @@
 #  --- loadbalancer/main ----
 
 resource "aws_lb" "main_lb" {
-    name = "main-alb"
-    subnets = var.public_subnets
-    security_groups = var.public_sg
-    idle_timeout = 400
-  
+  name            = "main-alb"
+  subnets         = var.public_subnets
+  security_groups = var.public_sg
+  idle_timeout    = 400
+
+}
+
+
+resource "aws_lb_target_group" "main-tf" {
+  name     = "main-lb-tg-${substr(uuid(), 0, 4)}"
+  port     = var.tg_port     #80
+  protocol = var.tg_protocol #HTTP
+  vpc_id   = var.vpc_id
+  #   make this dynamic for multiple target groups
+  health_check {
+    healthy_threshold   = var.lb_healthy_threshold   
+    unhealthy_threshold = var.lb_unhealthy_threshold 
+    timeout             = var.lb_timeout             
+    interval            = var.interval               
+  }
 }
--- loadbalancer/variables.tf
+++ loadbalancer/variables.tf
@@ -2,5 +2,13 @@
 
 variable "public_subnets" {}
 variable "public_sg" {
-    type = list
+  type = list(any)
 }
+
+variable "tg_port" {}
+variable "tg_protocol" {}
+variable "vpc_id" {}
+variable "lb_healthy_threshold" {}
+variable "lb_unhealthy_threshold" {}
+variable "lb_timeout" {}
+variable "interval" {}
