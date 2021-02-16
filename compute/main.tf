bc150a090e0a5f0b2514097203ebdfe7c48bd78e
rkrezov2048 <rkrezov@gmail.com>
2021-02-16 09:44:34 +0100

    added compute module

---
+++ compute/main.tf
@@ -0,0 +1,11 @@
+#  -----module/compute
+
+data "aws_ami" "server_ami" {
+  most_recent = true
+  owners = [ "099720109477 " ]
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
