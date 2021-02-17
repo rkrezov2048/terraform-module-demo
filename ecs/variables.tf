e850dbdf8d1bf8f90c08edf054bd2d9e41b72068
rkrezov2048 <rkrezov@gmail.com>
2021-02-17 14:05:49 +0100

    fixed variable

--- ecs/variables.tf
+++ ecs/variables.tf
@@ -5,9 +5,6 @@ variable "lc_iam_instance_profile" {}
 variable "lc_security_groups" {}
 variable "lc_associate_public_ip_address" {}
 variable "asg_min_size" {}
-
-
-variable "asg_min_size" {}
 variable "asg_max_size" {}
 variable "asg_desired_capacity" {}
 variable "asg_vpc_zone_identifier" {}
