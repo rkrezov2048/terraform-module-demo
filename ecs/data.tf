8764bf9381b00bbff4f6b5b3bfe03af1e9b3d666
rkrezov2048 <rkrezov@gmail.com>
2021-02-18 09:47:09 +0100

    added a data file

---
+++ ecs/data.tf
@@ -0,0 +1,21 @@
+# --- data/ecs-module -----
+
+data "aws_ami" "amazon_ecs" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami*amazon-ecs-optimized"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["amazon", "self"]
+}
--- ecs/main.tf
+++ ecs/main.tf
@@ -1,24 +1,4 @@
 # --- main/ecs ----
-data "aws_ami" "amazon_ecs" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn-ami*amazon-ecs-optimized"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["amazon", "self"]
-}
-
 
 resource "aws_launch_configuration" "ecs_lc" {
   name                        = "ecs-launch-config"
