89db0529a7e7a5042e1fe357e954b0836c5dae32
rkrezov2048 <rkrezov@gmail.com>
2021-03-03 17:40:28 +0100

    changed var

--- ecs-service/variables.tf
+++ ecs-service/variables.tf
@@ -2,10 +2,10 @@
 
 variable "ecs_task_family" {}
 variable "ecs_task_network_mode" {}
-variable "tags" {}
 variable "ecs_service_name" {}
 variable "ecs_service_desired_count" {}
 variable "ecs_service_launch_type" {}
 variable "ecs_service_cluster" {}
 variable "ecs_service_port" {}
 variable "aws_lb_target_group" {}
+variable "tags" {}
